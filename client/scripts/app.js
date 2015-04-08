dc6b30a5838712a6045e35bb6a2e69aba494ddfc
yuriybash <yuriybash@gmail.com>
2015-04-07 17:55:59 -0700

    finished basic jQuery implementation

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,6 @@
 // YOUR CODE HERE:
 var messages = {};
-
+var followedUsers = [];
 
 var ajax = {
   // always use this url
@@ -10,48 +10,71 @@ var ajax = {
   contentType: 'application/json',
   success: function (data) {
     //console.log(data.results[0].objectId);
+
+    var newMessages =[];
+
     for(var x =0; x < data.results.length; x++){
-        messages[data.results[x].objectId] = data.results[x]
+        if (!messages.hasOwnProperty(data.results[x].objectId)){
+          console.log("NEW MESSAGE")
+          newMessages.unshift(data.results[x]);
+        }
+        messages[data.results[x].objectId] = data.results[x];
     }
 
-    var sortableMessages = [];
 
 
-    for(var prop in messages){
-          if(messages[prop].text)messages[prop].text = escape(messages[prop].text).substr(0,140);
-      sortableMessages.push({
-                              "createdAt": messages[prop].createdAt,
-                              "user":messages[prop].username,
-                              "text":messages[prop].text,
-                              "roomname":messages[prop].roomname
-      })
-    }
 
-    sortableMessages.sort(function(a, b){
-        return Date.parse(a.createdAt) - Date.parse(b.createdAt)
-    })
 
-  _.each(sortableMessages, function(a,key){
-    $(".messages").prepend('<div class="modal-content">'+
+    // for(var prop in messages){
+    //       if(messages[prop].text)messages[prop].text = escape(messages[prop].text).substr(0,140);
+    //   sortableMessages.push({
+    //                           "createdAt": messages[prop].createdAt,
+    //                           "user":messages[prop].username,
+    //                           "text":messages[prop].text,
+    //                           "roomname":messages[prop].roomname
+    //   })
+    // }
+
+
+  _.each(newMessages, function(a,key){
+    $(".messages").prepend('<div class="message ' + (a.roomname || "") + ' ">'+
+      '<div class="modal-content ' + a.username + '">'+
       '<div class="modal-header">'+
-          '<h4 class="modal-title">'+a.username+'</h4>'+
+          '<h4 class="modal-title">'+ '<a>' + a.username+ '</a>' + '</h4>'+
         '</div>'+
         '<div class="modal-body">'+
-          '<p>'+a.text+'</p>'+
+          '<p>'+escape(a.text)+'</p>'+
         '</div>'+
         '<div class="modal-footer">'+
-          '<p>'+a.createdAt+'</p><p>'+a.roomname+'</p>'+
-          '<button type="button" class="btn btn-primary">Follow User</button>'+
+            '<p class="text-left ">sent: '+a.createdAt+' room: '+a.roomname+'</p>'+
+            '<button type="button" class="btn btn-primary followuserbutton ' + a.username +'">Follow User</button>'+
         '</div>'+
-      '</div><!-- /.modal-content -->');
-    //<div class="message ' + (a.roomname || "") + '" id='+key+'>'+key + " " + a.text+" " + a.username+ " " + a.createdAt + "ROOMNAME: " + a.roomname +'</div>');    if ( $('.message').length > 100 ) {
+      '</div><!-- /.modal-content -->'+
+      '</div>');
+    //<div class="message ' + (a.roomname || "") + '" id='+key+'>'+key + " " + a.text+" " + a.username+ " " + a.createdAt + "ROOMNAME: " + a.roomname +'</div>');
+    if ( $('.message').length > 100 ){
       $('.message').last().remove();
+    }
     if(window.filter && a.roomname !== window.filter){
       $('.message').first().toggle();
     }
 
-  });
+    if(followedUsers.indexOf(a.username) > -1){
+        $('.followuserbutton').first().text("Followed!")
+    }
 
+  });
+    $("body").on("click", ".followuserbutton", function(){
+        $(this).text("Followed!");
+        var numberOfClasses = $(this).attr('class').split(' ').length;
+        var username = ($(this).attr('class').split(' ')[numberOfClasses-1]);
+        followedUsers.push(username);
+        $(".followuserbutton."+username).text("Followed!");
+        $(this).parent().parent().find("p").first().css("font-weight", "Bold")
+        $("."+username+" .modal-body p").css("font-weight", "Bold");
+
+
+    })
   },
   error: function (data) {
     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -62,7 +85,7 @@ var ajax = {
 $.ajax(ajax);
 setInterval(function(){
   $.ajax(ajax);
-},50000000);
+},5000);
 
 
 function escape(string){
--- client/scripts/documentOnReady.js
+++ client/scripts/documentOnReady.js
@@ -2,86 +2,69 @@ var filter;
 
 $(document).ready(function(){
 
-    var $username;
-    var $messageToSend;
-
-    $("#mymodal").modal('show');
+  //$("body").on("click", )
 
 
-      $(".sendmessagebutton").on('click', function(){
+  var $username;
+  var $messageToSend;
 
-          $username = $('.username').val();
-          $messageToSend = $('.sendmessage').val();
+  $("#mymodal").modal('show');
 
-          $.ajax({
-            // always use this url
-            url: 'https://api.parse.com/1/classes/chatterbox',
-            type: 'POST',
-            data: JSON.stringify({'roomname': '6th floor', 'username':$username, 'text':$messageToSend}),
-            contentType: 'application/json',
-            success: function (data) {
-              console.log('chatterbox: Message sent');
-              console.log($messageToSend)
-            },
-            error: function (data) {
-              // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-              console.error('chatterbox: Failed to send message');
-            }
-          });
+  $(".sendmessagebutton").on('click', function(){
 
-      })
+    $username = $('.username').val();
+    $messageToSend = $('.sendmessage').val();
 
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify({'roomname': '6th floor', 'username':$username, 'text':$messageToSend}),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+        console.log($messageToSend)
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message');
+      }
+    });
 
+  });
 
+  $(".addroombutton").on("click", function(){
+    var $roomType = $(".addroom").val();
+    var $buttonText = $(".defaultbutton").text();
 
-      $(".addroombutton").on("click", function(){
-          var $roomType = $(".addroom").val();
+    if(!$("ul").children().is("."+$buttonText)){
+      $("ul").append("<li class="+$buttonText+"><a href=\"#\">"+$buttonText+"</a></li>");
+    }
+    if(!$("ul").children().is("."+$roomType)){
+      $("ul").append("<li class="+$roomType+"><a href=\"#\">"+$roomType+"</a></li>");
+    }
+    $(".defaultbutton").text($roomType);
 
-          var $buttonText = $(".defaultbutton").text();
+    $(".message").not("."+$roomType).hide();
+    filter = $roomType;
 
-          if(!$("ul").children().is("."+$buttonText)){
-              $("ul").append("<li class="+$buttonText+"><a href=\"#\">"+$buttonText+"</a></li>");
-          }
-          if(!$("ul").children().is("."+$roomType)){
-              $("ul").append("<li class="+$roomType+"><a href=\"#\">"+$roomType+"</a></li>");
-          }
-          $(".defaultbutton").text($roomType);
+    $("a").on("click", function(){
+      var clickedLink = $(this).text();
+      $(".defaultbutton").text(clickedLink);
+      if($(this).text()==="All Rooms"){
+        $(".message").show();
+        filter = undefined;
+      } else{
+        $("."+clickedLink).show();
+        $(".message").not("."+clickedLink).hide();      //use show and hide, right now we're never "un-hiding" previously toggled nodes
+        filter = $(this).text();
+      }
+    });
+  });
 
-          $(".message").not("."+$roomType).hide();
-          filter = $roomType;
-
-
-
-
-          $("a").on("click", function(){
-            var clickedLink = $(this).text();
-            $(".defaultbutton").text(clickedLink);
-            if($(this).text()==="All Rooms"){
-              $(".message").show();
-              filter = undefined;
-            } else{
-              $("."+clickedLink).show();
-              $(".message").not("."+clickedLink).hide();      //use show and hide, right now we're never "un-hiding" previously toggled nodes
-              filter = $(this).text();
-            }
-          })
-
-
-
-
-      })
-
-
-
-
-
-
-
-      $(".defaultbutton").on("click", function(){
-          if ($(this).val() === "All Rooms")
-            $(".message").show();
-          //filter = $(this).val();
-      })
+    $(".defaultbutton").on("click", function(){
+      if ($(this).val() === "All Rooms")
+        $(".message").show();
+            //filter = $(this).val();
+    });
 
 
 
@@ -91,4 +74,4 @@ $(document).ready(function(){
 
 
 
-})
+});
